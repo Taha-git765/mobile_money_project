a15589de4acdada294cdcac920ae7a0d9c4051e6
Taha-git765 <tahathiam123@gmail.com>
2026-03-29 05:58:44 +0100

    2eme kharou

--- services/ClientService.java
+++ services/ClientService.java
@@ -6,24 +6,10 @@ import models.Client;
 import dao.ClientDAO;
 
 public class ClientService {
-    private ClientDAO Cdao = new ClientDAO();
+    private ClientDAO clientDAO = new ClientDAO();
 
-    public void ajouterClient() /*throws SQLException*/{
+    public void ajouterClient(Client newCli) /*throws SQLException*/{
         // A ajouter: verifier si le telephone existe deja dans la base
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Entrer le nom du client:");
-        String nom = sc.next();
-        System.out.println("Entrer le prenom du client:");
-        String prenom = sc.nextLine();
-        System.out.println("Entrer le numero de telephone du client:");
-        String num_tel = sc.next();
-        System.out.println("Entrer l'adresse du client:");
-        String adresse = sc.nextLine();
-
-        Client newCli = new Client(nom, prenom, num_tel, adresse);
-
-        Cdao.ajouterCli_toBD(newCli);
-        sc.close();
-
+        clientDAO.ajouterCli_toBD(newCli);
     }
 }
--- ui/Menu.java
+++ ui/Menu.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import models.*;
@@ -10,21 +11,44 @@ import services.*;
 
 public class Menu {
     private Scanner in = new Scanner(System.in);
-    private ClientService cliService = new ClientService();
-    public void afficherMenu(){
+    private ClientService clientService = new ClientService();
+    public void start(){
         while(true){
-            System.out.println("1. Ajouter un client");
-            System.out.println("2. Quitter");
-
-            String choix = in.next();
-
-            if(choix.equals("1"))
-                cliService.ajouterClient();
-            else{
-                System.out.println("Bye!!!");
-                break;
+            afficherMenu();
+            String choix = in.nextLine();
+            switch (choix) {
+                case "1": ajouterClient(); break;
+                case "2": System.out.println("Au revoir"); return;
+                default: System.out.println("Choix invalide.");
             }
-            in.close();
         }
+    }
+
+    private void afficherMenu(){
+        System.out.println("\n=== Mobile Money ===");
+        System.out.println("1. Ajouter un client");
+        System.out.println("2. Quitter");
+        System.out.println("Votre choix: ");
+    }
+
+    private void ajouterClient(){
+        try{
+            System.out.println("Nom: ");
+            String nom = in.nextLine();
+            System.out.println("Prenom: ");
+            String prenom = in.nextLine();
+            System.out.println("Numero de telephone: ");
+            String tel = in.nextLine();
+            System.out.println("Adresse: ");
+            String adresse = in.nextLine();
+
+            Client newCli = new Client(nom, prenom, tel, adresse);
+            clientService.ajouterClient(newCli);
+            System.out.println("Client ajoute avec succes!");
+        }
+        catch(Exception e){
+            System.out.println("Erreur: " + e.getMessage());
+        }
+
     }
 }
